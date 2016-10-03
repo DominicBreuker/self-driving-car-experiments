0298703e59dd20d2fbc39ce006c7ee1b37216ff5
Dominic Breuker <dominic.breuker@gmail.com>
2016-10-03 15:58:52 +0200

    make get_data script debian-friendly

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -3,4 +3,4 @@ echo "using "$DATA_DIR" as data dir"
 mkdir -p $DATA_DIR
 
 wget --continue -O $DATA_DIR/dataset.bag.tar.gz http://bit.ly/udacity-dataset-2-1
-unzip tar xvfz $DATA_DIR/dataset.bag.tar.gz
+tar -xzf $DATA_DIR/dataset.bag.tar.gz -C $DATA_DIR/
